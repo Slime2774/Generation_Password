15eb75b7f77a258bd1150c15e11cbc9a1182dc5a
Slime2774 <rezsikove@gmail.com>
2026-07-16 15:26:07 +0300

    Feat: Добавлен .gitignore для удаления мусора кода + реализована базавая генерация без спец символов

---
+++ GenerationPassword/GenerationPassword/Generator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenerationPassword
+{
+    class Password
+    {
+        public string GeneratePass(int length)
+        {
+            Random rnd = new Random();
+            StringBuilder password = new StringBuilder();
+            string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string specialChars = "1234567890!@#$%^&*()_+-=[]{}|;':\",./<>?";
+
+            Console.WriteLine("Хотите ли вы использовать цифры и спец.знаки");
+            Console.Write("Введите ДА или НЕТ: ");
+            string answer = Console.ReadLine();
+
+            if (answer == "ДА")
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    password.Append(chars[rnd.Next(chars.Length)] + specialChars[rnd.Next(specialChars.Length)]);
+                }
+                return password.ToString();
+            }
+            else 
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    password.Append(chars[rnd.Next(chars.Length)]);
+                }
+                return password.ToString();
+            }
+
+        }
+    }
+}
---
+++ GenerationPassword/GenerationPassword/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenerationPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Password pas = new Password();
+            while (true)
+            {
+
+                Console.Write("Введите длину пароля: ");
+                int lengthPassword = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(pas.GeneratePass(lengthPassword));
+
+                Console.WriteLine("Нажмите любую кнопку для продолжения\n\n");
+                Console.ReadKey();
+            }
+        }
+    }
+}
